0e201c726dbf8cabd14fa97ca85116cd3cdbf791
Carlos Velez <cfvelez@macbook-pro-de-carlos.local>
2016-03-20 17:59:21 -0500

    Initial Commit

---
+++ PracticaUno/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticaUno
+//
+//  Created by Carlos Velez on 20/03/16.
+//  Copyright © 2016 Carlos Velez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
